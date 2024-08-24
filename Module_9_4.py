dd07138be8184cd2d25e07c968211f2776140c71
Mihail-lozovoy <lozovoy_michael@inbox.ru>
2024-08-24 13:34:34 +0300

    Add files via upload

--- Module_9_4.py
+++ Module_9_4.py
@@ -1,3 +1,21 @@
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+print(list(map(lambda x, y: x == y, first, second)))
+
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, mode='w', encoding='utf-8') as file:
+            for i in data_set:
+                file.write(f"{i}\n")
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
 from random import choice
 
 
@@ -13,3 +31,5 @@ first_ball = MysticBall('Да', 'Нет', 'Наверное')
 print(first_ball())
 print(first_ball())
 print(first_ball())
+
+from random import choice
